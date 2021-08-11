d2fb35061e153e054542ac166be423856e8ea2b1
Bogachev Dmitrii <dm.bogachev@yandex.ru>
2021-08-11 13:27:13 +0300

    Added simple TK GUI

--- SimpleServer/SimpleServer.py
+++ SimpleServer/SimpleServer.py
@@ -1,10 +1,12 @@
 import socket
 from threading import Thread
 
+import ctypes
 class Server(Thread):
     """A simple TCP/IP Server which receives data from client and redirect it to all other connected clients"""
     
     clients = []
+    threads = []
 
     def __init__(self, host='localhost', port=48569, timeout=60, sname='TCP/IP Server'):
         self.host = host
@@ -12,47 +14,60 @@ class Server(Thread):
         self.timeout = timeout
         self.sname = sname
         Thread.__init__(self)
-        print(self.sname + ' was initialized')
+        self.print(self.sname + ' was initialized')
 
     def run(self):
-        print(self.sname + ' was started')
+        self.print(self.sname + ' was started')
         self.listen()
+
+    def stop(self):
+        pass
 
     def bind(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.host, self.port))
-        print(' '.join([self.sname, 'was bound to IP:', self.host, 'Port:', str(self.port)]))
+        self.print([self.sname, 'was bound to IP:', self.host, 'Port:', str(self.port)])
         
     def listen(self):
         self.bind()
         self.sock.listen(5)
-        print(self.sname + ' is listening')
+        self.print(self.sname + ' is listening')
 
         while True:
             client, address = self.sock.accept()
             client.settimeout(self.timeout)
             self.clients.append(client)
-            Thread(target=self.client_handle, args=(client, address,)).start()
+            thread = Thread(target=self.client_handle, args=(client, address,)).start()
+            self.threads.append(thread)
+
+    def on_receive(self, client_ip=''):
+        self.print(['Received data from', client_ip + ':', self.data.rstrip('\n')])
+
+    def print(self, msg):
+        if type(msg) == type([]):
+            print(' '.join(msg))
+        if type(msg) == type(''):
+            print(msg)
 
     def client_handle(self, client, address):
         buf_size = 4096
 
-        print(' '.join(['Client connected with IP:', address[0], 'Port:', str(address[1])]))
+        self.print(['Client connected with IP:', address[0], 'Port:', str(address[1])])
 
         while True:
             try:
-                data = client.recv(buf_size).decode('utf-8')
-                if data:
-                    print(' '.join(['Received data from', address[0] + ':', data.rstrip('\n')]))
+                self.data = client.recv(buf_size).decode('utf-8')
+                if self.data:
+                    self.on_receive(address[0])
                     for item in self.clients:
                         if item != client:
-                            item.send(data.encode('utf-8'))
+                            item.send(self.data.encode('utf-8'))
                 else:
                     raise error('Client disconnected')
 
-            except:
-                print(' '.join(['Client with IP:', address[0], 'was disconnected!']))
+            except Exception as e:
+                self.print(['Client with IP:', address[0], 'was disconnected!'])
                 self.clients.remove(client)
                 client.close()
                 return False
---
+++ SimpleServer/SimpleServerGUI.py
@@ -0,0 +1,87 @@
+from SimpleServer import *
+import tkinter as tk
+import tkinter.ttk as ttk
+from datetime import datetime
+import os
+
+class ServerGUIExtension(Server):
+
+    def __init__(self, host='localhost', port=48569, timeout=60, sname='TCP/IP Server', tkroot=None):
+        self.tkroot = tkroot
+        if tkroot is not None:
+            self.tkroot.title("Server log")
+            self.tk_init_treeview()
+            self.tk_init_listbox()
+        return super().__init__(host=host, port=port, timeout=timeout, sname=sname)
+
+    def on_receive(self, client_ip):
+        if (self.tkroot != None):
+            self.receive_log.insert("", tk.END, values=(datetime.now().strftime('%H:%M:%S'), 
+                                                        client_ip, 
+                                                        self.data))
+            self.receive_log.yview_moveto(1)
+
+    def print(self, msg):
+        if self.tkroot is not None:
+            if type(msg) == type([]):
+                self.connect_log.insert(tk.END, datetime.now().strftime('%H:%M:%S') + ' ' + ' '.join(msg))
+            if type(msg) == type(''):
+                self.connect_log.insert(tk.END, datetime.now().strftime('%H:%M:%S') + ' ' + msg)
+            self.connect_log.see(tk.END)
+        else:
+            return super().print(msg=msg)
+
+    def tk_init_listbox(self):
+        self.connect_log = tk.Listbox(self.tkroot)
+        ysb = ttk.Scrollbar(self.tkroot, orient="vertical",command=self.connect_log.yview)
+        self.connect_log.configure(yscrollcommand=ysb.set)
+        self.connect_log.grid(row=0, column=0, sticky=tk.N + tk.S + tk.E + tk.W)
+        ysb.grid(row=0, column=1, sticky=tk.N + tk.S)
+
+    def tk_init_treeview(self):
+        self.receive_log = ttk.Treeview(self.tkroot)
+        ysb = ttk.Scrollbar(self.tkroot, orient="vertical",command=self.receive_log.yview)
+        self.receive_log.configure(yscrollcommand=ysb.set)
+        self.receive_log['columns']=('Time', 'From', 'Data')
+        self.receive_log.column('#0', width=0, stretch=tk.NO)
+        self.receive_log.column('Time', anchor=tk.CENTER, width=60)
+        self.receive_log.column('From', anchor=tk.CENTER, width=80)
+        self.receive_log.column('Data', width=1000)
+        self.receive_log.heading('#0', text='', anchor=tk.CENTER)
+        self.receive_log.heading('Time', text='Time', anchor=tk.CENTER)
+        self.receive_log.heading('From', text='From', anchor=tk.CENTER)
+        self.receive_log.heading('Data', text='Data')
+        self.receive_log.grid(row=1, column=0, sticky=tk.N + tk.S + tk.E + tk.W)
+        ysb.grid(row=1, column=1, sticky=tk.N + tk.S)
+
+
+def tk_init_connectionwindow():
+    host_label = tk.Label(con_root, text='Host:').grid(row=0, column=0)
+    host = socket.gethostbyname(socket.gethostname())
+    host_entry = tk.Entry(con_root, width=30, textvariable=host).grid(row=0, column=1, columnspan=3)
+    
+def callback():
+    global log_root, server
+    log_root.destroy()
+
+if __name__ == '__main__':
+    log_root = tk.Tk()
+    log_root.resizable(0, 0)
+    log_root.protocol('WM_DELETE_WINDOW', callback)
+    host = socket.gethostbyname(socket.gethostname())
+    server = ServerGUIExtension(host=host,
+                port=48569, 
+                timeout=None, 
+                sname='CoffeeRobot TCP/IP Server',
+                tkroot=log_root)
+    server.start()
+    log_root.mainloop()
+    print('Now you can close this window')
+
+
+    
+
+
+
+
+
